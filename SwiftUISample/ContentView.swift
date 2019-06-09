8f8baa19ad881d85725c9fb5f05e57ed2b556417
tosaka07 <tosaka.tech@gmail.com>
2019-06-09 19:29:36 +0900

    Initial Commit

---
+++ SwiftUISample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUISample
+//
+//  Created by 坂上翔悟 on 2019/06/09.
+//  Copyright © 2019 Shogo Sakaue. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
